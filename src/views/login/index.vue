16c226d03bb200e0a270e19da299eaa8640be0ca
陈阳 <1742878501@qq.com>
2021-02-24 10:27:51 +0800

    2.登录基本结构

---
+++ src/views/login/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="login-container">
+    <!-- 导航栏 -->
+    <van-nav-bar title="登录" />
+    <!-- /导航栏 -->
+    <!-- 登录表单 -->
+    <van-form>
+      <van-field name="用户名" placeholder="请输入手机号" />
+      <van-field type="password" name="验证码" placeholder="验证码" />
+      <div style="margin: 16px;">
+        <van-button block type="info" native-type="submit">登录</van-button>
+      </div>
+    </van-form>
+    <!-- /登录表单 -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginPage',
+
+  components: {},
+  props: {},
+  data () {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created () {},
+  mounted () {},
+  methods: {}
+}
+</script>
+
+<style lang="less" scoped></style>
